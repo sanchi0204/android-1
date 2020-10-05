ba1472e5dca4142372ea476e312ff01124bcc574
Abel García de Prada <agdeprada@izertis.com>
2020-10-05 13:47:17 +0200

    Start moving shared preferences to preference provider

--- owncloudData/src/main/java/com/owncloud/android/data/preferences/datasources/SharedPreferencesProvider.kt
+++ owncloudData/src/main/java/com/owncloud/android/data/preferences/datasources/SharedPreferencesProvider.kt
@@ -24,6 +24,9 @@ interface SharedPreferencesProvider {
     fun putString(key: String, value: String)
     fun getString(key: String, defaultValue: String): String?
 
+    fun putInt(key: String, value: Int)
+    fun getInt(key: String, defaultValue: Int): Int
+
     fun putBoolean(key: String, value: Boolean)
     fun getBoolean(key: String, defaultValue: Boolean): Boolean
 }
